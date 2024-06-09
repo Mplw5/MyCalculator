f09ece0a707a33741bdc9addd6ddf7616178405a
yplw <18552548565@163.com>
2024-06-09 20:53:36 +0800

    增加改变字体颜色

---
+++ app/src/main/java/com/example/mycalculator/Background.java
@@ -0,0 +1,51 @@
+package com.example.mycalculator;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Background extends AppCompatActivity {
+    ImageButton img1,img2,img3,img4,back;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.background_activity);
+
+        ImageButton img1 = findViewById(R.id.imageButton);
+        img1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        ImageButton img2 = findViewById(R.id.imageButton);
+        img1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        ImageButton img3 = findViewById(R.id.imageButton);
+        img1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        ImageButton img4 = findViewById(R.id.imageButton);
+        img1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+    }public void back(View v) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivityForResult(intent,3);
+    }
+}
